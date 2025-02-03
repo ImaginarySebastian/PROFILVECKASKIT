86035665d8f6f4b18c4c62b1506f74f98a65d527
Artemii_Polishchuk <ilovecrowandsendy@gmail.com>
2025-02-03 11:39:10 +0100

    Added Re-Set

--- Profile/Assets/Artemii/Scripts/PlayerHealth.cs
+++ Profile/Assets/Artemii/Scripts/PlayerHealth.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -29,7 +30,8 @@ public class PlayerHealth : MonoBehaviour
             }
             else
             {
-                Destroy(gameObject);
+                int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+                SceneManager.LoadScene(currentSceneIndex);
             }
         }
     }
