a95934aa4264f749e2ace29e6552e40101312bb1
Artemii_Polishchuk <ilovecrowandsendy@gmail.com>
2025-02-04 10:08:45 +0100

    Adderade Dash
    Men det finns bug att du stannar i luften när du använder det om du rör det sig inte

--- Profile/Assets/Artemii/Scripts/PlayerMovements.cs
+++ Profile/Assets/Artemii/Scripts/PlayerMovements.cs
@@ -10,12 +10,19 @@ public class PlayerMovements : MonoBehaviour
     [SerializeField] float moveSpeed = 10;
     [SerializeField] float jumpSpeed = 3;
     [SerializeField] ContactFilter2D groundFilter;
+    [SerializeField] float dashSpeed;
+    [SerializeField] float dashDuration;
+    [SerializeField] float dashCooldown;
+    bool canDash = true;
     Vector2 moveInput;
     Rigidbody2D rb;
     bool isGrounded = true;
+    float originalGravity;
+    float lastDirection = 1;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        originalGravity = rb.gravityScale;
     }
     private void OnMove(InputValue value)
     {
@@ -37,10 +44,33 @@ public class PlayerMovements : MonoBehaviour
             transform.localScale = new Vector2(Mathf.Sign(moveInput.x), transform.localScale.y); 
         }
     }
+    private IEnumerator Dash()
+    {
+        if (!canDash) yield break;
+        canDash = false;
+        float dashDirection = moveInput.x != 0 ? Mathf.Sign(moveInput.x) : lastDirection;
+        lastDirection = dashDirection; 
 
+        rb.gravityScale = 0; 
+        rb.velocity = new Vector2(dashDirection * dashSpeed, 0);
+        yield return new WaitForSeconds(dashDuration); 
+
+        rb.gravityScale = originalGravity; 
+        rb.velocity = new Vector2(0, rb.velocity.y); 
+        yield return new WaitForSeconds(dashCooldown); 
+        canDash = true; 
+    }
+    private void OnDash()
+    {   
+        StartCoroutine(Dash());
+    }
     // Update is called once per frame
     void Update()
     {
+        if (moveInput.x != 0)
+        {
+            lastDirection = moveInput.x;
+        }
         Run();
     }
     private void FixedUpdate()
