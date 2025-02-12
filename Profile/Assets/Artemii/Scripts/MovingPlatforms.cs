73b79f92e56beff7bd748340e75c12a66408c723
Elida3333 <elida.ekelund@elev.ga.lbs.se>
2025-02-12 13:04:23 +0100

    Level1 and Spikes

--- Profile/Assets/Artemii/Scripts/MovingPlatforms.cs
+++ Profile/Assets/Artemii/Scripts/MovingPlatforms.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.TestTools;
 
 public class MovingPlatforms : MonoBehaviour
 {
@@ -12,17 +13,28 @@ public class MovingPlatforms : MonoBehaviour
     Rigidbody2D rb;
     Rigidbody2D rbPlayer;
     private bool Onplatform = false;
+    public float Fliptime;
+    public float Flip = 3f;
+    public float moveRange = 5f;
+    private Vector2 startpos;
+
+  
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         col = GetComponent<BoxCollider2D>();
         RandomValues();
         rbPlayer = Player.GetComponent<Rigidbody2D>();
+    
     }
     void Update()
     {
         MovePlatforms();
     }
+
+  
+
+
     private void RandomValues()
     {
         direction = Random.Range(1,3);
@@ -38,6 +50,7 @@ public class MovingPlatforms : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             Onplatform = true;
+            AttachPlayerToPlatform();
         }
     }
 
@@ -46,9 +59,14 @@ public class MovingPlatforms : MonoBehaviour
         if (Onplatform && collision.CompareTag("Player"))
         {
             rbPlayer.velocity = new Vector2(speed, rbPlayer.velocity.y);
-
             rbPlayer.gravityScale = 0;
+        }
 
+
+        if (Onplatform && collision.CompareTag("Player"))
+        {
+            rbPlayer.velocity = new Vector2(speed, rbPlayer.velocity.x);
+            rbPlayer.gravityScale = 0;
 
         }
     }
@@ -58,14 +76,46 @@ public class MovingPlatforms : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             Onplatform = false;
-            
-            rbPlayer.gravityScale = 1;
+            DetachPlayerFromPlatform();
+
+             rbPlayer.gravityScale = 1;
         }
     }
 
     void MovePlatforms()
     {
         rb.velocity = new Vector2(speed, rb.velocity.y);
+    }
+
+    void DetachPlayerFromPlatform()
+    {
+        Player.transform.parent = null;
+    }
+
+    void AttachPlayerToPlatform()
+    {
+        Player.transform.parent = transform;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(speed, 0f);
+        float newPosX = Mathf.PingPong(Time.time * speed, moveRange);
+        transform.position = new Vector2(startpos.x + newPosX, transform.position.y);
+
+        if (Time.time >= Fliptime)
+        {
+            Flipp();
+            Fliptime = Time.time + Flip;
+        }
+    }
+
+    void Flipp()
+    {
+        Vector2 scale = transform.localScale;
+        scale.x = -scale.x;
+        transform.localScale = scale;
+
     }
 }
 
